1545a7b94878a90f16277d59f23be27fa195ef4d
Vinh Hoang <vinhtienle004@gmail.com>
2025-09-09 15:20:06 +0700

    WishList

--- WebBanHang/wwwroot/js/Cart.js
+++ WebBanHang/wwwroot/js/Cart.js
@@ -38,3 +38,40 @@
         });
     });
 });
+// Thêm vào danh sách yêu thích
+$(document).ready(function () {
+    $(".add-to-wishlist").click(function (e) {
+        e.preventDefault();
+
+        var id = $(this).data("id");
+
+        $.ajax({
+            url: '/WishList/AddToWishList',
+            type: 'POST',
+            data: { id: id },
+            success: function (res) {
+                // Hiển thị thông báo thành công bằng SweetAlert2
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Thêm sản phẩm thành công!',
+                    text: `Sản phẩm đã được thêm vào danh sách yêu thích.`,
+                    showConfirmButton: false,
+                    timer: 1500, // tự ẩn sau 1.5 giây
+                    position: 'top',
+                    toast: true
+                });
+            },
+            error: function () {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Có lỗi!',
+                    text: 'Không thể thêm sản phẩm vào danh sách yêu thich.',
+                    timer: 1500,
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false
+                });
+            }
+        });
+    });
+});
