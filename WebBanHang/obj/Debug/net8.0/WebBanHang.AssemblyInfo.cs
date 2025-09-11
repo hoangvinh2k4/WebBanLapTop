80a8d1d17c7a34b014936caf6246ec344ca96b1f
Vinh Hoang <vinhtienle004@gmail.com>
2025-09-11 16:30:52 +0700

    abc

--- WebBanHang/obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ WebBanHang/obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2164f6a22134513a82994850b4ecc2d93e90af5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4019dcd93647f5608a16b71df1c2faa2ab2d6dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
